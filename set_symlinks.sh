f3e8a421be277840de202815546580ba7b74a76c
kostas230 <57586901+kostas230@users.noreply.github.com>
2021-08-30 14:43:17 +0300

    Update set_symlinks.sh

--- set_symlinks.sh
+++ set_symlinks.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-ln -sf ~/.config/home_scripts_linux/xinitrc ~/.xinitrc
-ln -sf ~/.config/home_scripts_linux/xinitrc ~/.xsession
-ln -sf ~/.config/home_scripts_linux/zprofile ~/.zprofile
-ln -sf ~/.config/home_scripts_linux/Xresources ~/.Xresources
-ln -sf ~/.config/home_scripts_linux/cwmrc ~/.cwmrc
-ln -sf ~/.config/home_scripts_linux/vimrc ~/.vimrc
-ln -sf ~/.config/home_scripts_linux/robbyrussell-new.zsh-theme ~/.oh-my-zsh/themes/robbyrussell-new.zsh-theme
+ln -sf ~/.config/home_scripts/xinitrc ~/.xinitrc
+ln -sf ~/.config/home_scripts/xinitrc ~/.xsession
+ln -sf ~/.config/home_scripts/zprofile ~/.zprofile
+ln -sf ~/.config/home_scripts/Xresources ~/.Xresources
+ln -sf ~/.config/home_scripts/cwmrc ~/.cwmrc
+ln -sf ~/.config/home_scripts/vimrc ~/.vimrc
+ln -sf ~/.config/home_scripts/robbyrussell-new.zsh-theme ~/.oh-my-zsh/themes/robbyrussell-new.zsh-theme
+
+sudo cp ~/.config/home_scripts/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
+sudo cp ~/.config/home_scripts/20-intel.conf /etc/X11/xorg.conf.d/20-intel.conf
