1561add173e050dbca5747cc5a22e29769455033
kostas230 <57586901+kostas230@users.noreply.github.com>
2021-08-30 14:38:27 +0300

    Update set_symlinks.sh

--- set_symlinks.sh
+++ set_symlinks.sh
@@ -4,5 +4,4 @@ ln -sf ~/.config/home_scripts_linux/xinitrc ~/.xsession
 ln -sf ~/.config/home_scripts_linux/zprofile ~/.zprofile
 ln -sf ~/.config/home_scripts_linux/Xresources ~/.Xresources
 ln -sf ~/.config/home_scripts_linux/cwmrc ~/.cwmrc
-ln -sf ~/.config/home_scripts_linux/vimrc ~/.vimrc
 ln -sf ~/.config/home_scripts_linux/robbyrussell-new.zsh-theme ~/.oh-my-zsh/themes/robbyrussell-new.zsh-theme
