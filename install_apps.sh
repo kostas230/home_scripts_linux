cfaacf62f0be8ae2da43483b170db22ce4f26ae3
kostas230 <57586901+kostas230@users.noreply.github.com>
2022-12-17 20:43:45 +0000

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sudo pacman -S openssh wpa_supplicant \
-wireless_tools netctl dialog lvm2 grub efibootmgr dosfstools mtools zsh \
-os-prober wget curl git apparmor unrar unzip cups vim ranger zathura w3m \
-xf86-video-amdgpu mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon wine \
-libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau steam \
-mpv libreoffice chromium geeqie gimp electron telegram-desktop rtorrent picom \
+sudo pacman -S openssh wpa_supplicant git curl wget dosfstools \
+wireless_tools netctl dialog lvm2 grub efibootmgr mtools zsh zathura \
+os-prober wget curl git apparmor unrar unzip cups vim ranger w3m wine \
+xf86-video-amdgpu mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon steam \
+libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau picom \
+mpv libreoffice chromium geeqie gimp electron telegram-desktop rtorrent \
 xorg xorg-server xorg-apps xorg-xinit tint2 telegram-desktop thunderbird 
