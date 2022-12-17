fa355bfd26572a6de596e72da658e3110aa70cff
kostas230 <57586901+kostas230@users.noreply.github.com>
2022-12-17 20:18:19 +0000

    Add files via upload

---
+++ font.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo pacman -S tamsyn-font terminus-font ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family \
+ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-monofur adobe-source-code-pro-fonts ttf-opensans gentium-plus-font ttf-junicode xorg-fonts-type1 otf-latin-modern otf-latinmodern-math
