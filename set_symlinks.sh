297f1d7b5362c273fc29655937c9d2bde8b1b289
kostas230 <57586901+kostas230@users.noreply.github.com>
2022-12-17 20:38:18 +0000

    Update set_symlinks.sh

--- set_symlinks.sh
+++ set_symlinks.sh
@@ -6,6 +6,3 @@ ln -sf ~/.config/home_scripts/Xresources ~/.Xresources
 ln -sf ~/.config/home_scripts/cwmrc ~/.cwmrc
 ln -sf ~/.config/home_scripts/vimrc ~/.vimrc
 ln -sf ~/.config/home_scripts/robbyrussell-new.zsh-theme ~/.oh-my-zsh/themes/robbyrussell-new.zsh-theme
-
-sudo cp ~/.config/home_scripts/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
-sudo cp ~/.config/home_scripts/20-intel.conf /etc/X11/xorg.conf.d/20-intel.conf
