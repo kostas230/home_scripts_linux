a0bd54e216124dba91dfe5729fc5bb3e0f250601
kostas230 <57586901+kostas230@users.noreply.github.com>
2022-12-17 20:37:33 +0000

    Add files via upload

---
+++ install_apps.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo pacman -S openssh wpa_supplicant \
+wireless_tools netctl dialog lvm2 grub efibootmgr dosfstools mtools zsh \
+os-prober wget curl git apparmor unrar unzip cups vim ranger zathura w3m \
+xf86-video-amdgpu mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon wine \
+libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau steam \
+mpv libreoffice chromium geeqie gimp electron telegram-desktop rtorrent picom \
+xorg xorg-server xorg-apps xorg-xinit tint2 telegram-desktop thunderbird 
