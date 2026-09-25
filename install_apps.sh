5ea938ec2609f3294336e68d760d7d6ff82842d6
kostas230 <57586901+kostas230@users.noreply.github.com>
2026-09-25 20:55:36 +0000

    Delete install_apps.sh

--- install_apps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo pacman -S openssh wpa_supplicant git curl wget dosfstools vim \
-wireless_tools netctl dialog lvm2 grub efibootmgr mtools zsh zathura \
-os-prober wget curl git apparmor unrar unzip cups vim ranger w3m wine \
-xf86-video-amdgpu mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon steam \
-libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau picom \
-mpv libreoffice chromium geeqie gimp electron telegram-desktop rtorrent \
-xorg xorg-server xorg-apps xorg-xinit tint2 telegram-desktop thunderbird 
